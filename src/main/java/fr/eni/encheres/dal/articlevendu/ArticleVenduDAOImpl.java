90dd55d7fdbae3f5af06a6a716b34b0b0eb66610
GAB2022eni <gabriel.gastine2022@campus-eni.fr>
2022-03-30 17:01:08 +0200

    fin articlevendudaoiml

--- src/main/java/fr/eni/encheres/dal/articlevendu/ArticleVenduDAOImpl.java
+++ src/main/java/fr/eni/encheres/dal/articlevendu/ArticleVenduDAOImpl.java
@@ -4,7 +4,6 @@
 package fr.eni.encheres.dal.articlevendu;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,9 +12,9 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
 import fr.eni.encheres.bo.ArticleVendu;
 import fr.eni.encheres.bo.Categorie;
-import fr.eni.encheres.bo.Enchere;
 import fr.eni.encheres.bo.Retrait;
 import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dal.DALException;
@@ -78,14 +77,22 @@ public class ArticleVenduDAOImpl implements ArticleVenduDAO {
 	 * {@inheritedDoc}
 	 */
 	@Override
-	public void selectByIdArticleVendu(ArticleVendu articlevendu) throws DALException {
-		// TODO Auto-generated method stub
+	public ArticleVendu selectByIdArticleVendu(Integer IDArticleVendu) throws DALException {
+		ArticleVendu result = new ArticleVendu();
 		try (Connection con = ConnectionProvider.getConnection()) {
 			PreparedStatement stmt = con.prepareStatement(SELECT);
 			ResultSet rs = stmt.executeQuery();
 
+			while (rs.next()) {
+
+				result = itemBuilder(rs);
+			}
 		}
 
+		catch (SQLException e) {
+			throw new DALException("pb lors de la méthode selectByIdArticleVendu" + e.getMessage());
+		}
+		return result;
 	}
 
 	/**
@@ -98,14 +105,14 @@ public class ArticleVenduDAOImpl implements ArticleVenduDAO {
 			PreparedStatement stmt = con.prepareStatement(SELECTALL);
 			ResultSet rs = stmt.executeQuery();
 			while (rs.next()) {
-				ArticleVendu articlevendu = new ArticleVendu(rs.getString("nomArticle"), rs.getString("description"),
-						rs.getDate("dateDebutEncheres").toLocalDate(), rs.getDate("dateFinEncheres").toLocalDate(),
-						rs.getInt("miseAPrix"), rs.getInt("prixVente"), rs.getString("etatVente"),
-				articlevendu.setNoArticle(rs.getInt("noArticle"), articlevendu.setNoUtilisateur(rs.getInt("noUtilisateur"), articlevendu.setNoArticle(rs.getInt("noArticle"));
-				result.add(articlevendu);
-			}
 
-		} catch (SQLException e) {
+				ArticleVendu item = null;
+				item = itemBuilder(rs);
+				result.add(item);
+			}
+		}
+
+		catch (SQLException e) {
 			throw new DALException("pb lors de la méthode selectAllArticleVendu" + e.getMessage());
 		}
 		return result;
@@ -115,12 +122,13 @@ public class ArticleVenduDAOImpl implements ArticleVenduDAO {
 	 * {@inheritedDoc}
 	 */
 	@Override
-	public void deleteArticleVendu(ArticleVendu articlevendu) throws DALException {
-
+	public void deleteArticleVendu(ArticleVendu articleVendu) throws DALException {
 		try {
 			Connection con = ConnectionProvider.getConnection();
 			PreparedStatement stmt = con.prepareStatement(DELETE);
-			ResultSet rs = stmt.executeQuery();
+
+			stmt.setInt(1, articleVendu.getNoArticle());
+			stmt.executeUpdate();
 
 		} catch (SQLException e) {
 			throw new DALException("pb lors de la méthode deleteArticleVendu" + e.getMessage());
@@ -157,8 +165,8 @@ public class ArticleVenduDAOImpl implements ArticleVenduDAO {
 		RetraitDAO daoRetrait = DAOFactory.getRetraitDAO();
 		CategorieDAO daoCategorie = DAOFactory.getCategorieDAO();
 		UtilisateurDAO daoUtilisateur = DAOFactory.getUtilisateurDAO();
-		ArticleVendu result = new ArticleVendu();
 
+		ArticleVendu result = null;
 		Integer noArticle = rs.getInt("no_article");
 		String nomArticle = rs.getString("nom_article");
 		String description = rs.getString("description");
@@ -167,37 +175,31 @@ public class ArticleVenduDAOImpl implements ArticleVenduDAO {
 		Integer miseAPrix = rs.getInt("mise_initial");
 		Integer prixVente = rs.getInt("prix_vente");
 		String etatVente = (String) rs.getObject("no_article");
-		
+
 		Retrait lieuRetrait;
 		Categorie categorie;
 		Utilisateur utilisateur;
-		
+
 		try {
-			
+
 			lieuRetrait = daoRetrait.selectByIdRetrait(rs.getInt("no_article"));
 			categorie = daoCategorie.selectByIdCategorie(rs.getInt("no_categorie"));
 			utilisateur = daoUtilisateur.selectByIDUtilisateur(rs.getInt("no_utilisateur"));
-			
-				    
-		    
-			
-			
-			
+
 		} catch (DALException | SQLException e) {
 			throw new DALException("erreur dans l'itembuilder" + e.getMessage());
 		}
-		
-	
-		
 
-		
-		
-		
-		if (lieuRetrait != null) {
+		if (lieuRetrait == null) {
 
+			result = new ArticleVendu(nomArticle, description, dateDebutEncheres, dateFinEncheres, miseAPrix, prixVente,
+					etatVente, categorie, utilisateur);
+			result.setNoArticle(noArticle);
+		} else {
+			result = new ArticleVendu(nomArticle, description, dateDebutEncheres, dateFinEncheres, miseAPrix, prixVente,
+					etatVente, lieuRetrait, categorie, utilisateur);
+			result.setNoArticle(noArticle);
 		}
-
 		return result;
 	}
-
 }
