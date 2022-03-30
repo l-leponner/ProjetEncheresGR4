353245143f9b3ebf1a2eea413e273c76d16a7e03
LegeasArthur <Legeas.arthur@gmail.com>
2022-03-30 09:10:01 +0200

    Récupération de la DAOFAC

---
+++ src/main/java/fr/eni/encheres/dal/articlevendu/ArticleVenduDAO.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package fr.eni.encheres.dal.articlevendu;
+
+import java.util.List;
+
+import fr.eni.encheres.bo.ArticleVendu;
+import fr.eni.encheres.dal.DALException;
+
+/**
+ * Classe en charge de
+ * 
+ * @author ggastine2022
+ * @date 29 mars 2022
+ * @version ProjetEncheresGR4- V0.1
+ * @since 29 mars 2022 - 14:02:18
+ *
+ */
+public interface ArticleVenduDAO {
+
+	public void insertArticleVendu(ArticleVendu articlevendu) throws DALException;
+
+	public void selectByIdArticleVendu(ArticleVendu articlevendu) throws DALException;
+
+	public List<ArticleVendu> selectAllArticleVendu() throws DALException;
+
+	public void deleteArticleVendu(ArticleVendu articlevendu) throws DALException;
+
+	public void updateArticleVendu(ArticleVendu articlevendu) throws DALException;
+
+}
