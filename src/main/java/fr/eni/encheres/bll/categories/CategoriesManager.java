c974b6fd361985c38e864f5ff55d83db9261709e
LegeasArthur <Legeas.arthur@gmail.com>
2022-03-30 15:41:27 +0200

    Ajout du squelette de la BLLCategorie

---
+++ src/main/java/fr/eni/encheres/bll/categories/CategoriesManager.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package fr.eni.encheres.bll.categories;
+
+import fr.eni.encheres.bo.Categorie;
+
+/**
+ * Class en charge de 
+ * @author alegeas2022
+ * @date 30 mars 2022
+ */
+public interface CategoriesManager {
+
+	public void addCategorie(Categorie categorie) throws BLLException;
+	
+	
+}
