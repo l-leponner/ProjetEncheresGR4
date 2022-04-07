49c0d8db52079bb0f45af4543c131ed8996ac714
LegeasArthur <Legeas.arthur@gmail.com>
2022-04-07 09:12:11 +0200

    mise a jour

--- src/main/java/fr/eni/encheres/bll/articleVendu/ArticleVenduBLLManager.java
+++ src/main/java/fr/eni/encheres/bll/articleVendu/ArticleVenduBLLManager.java
@@ -40,16 +40,16 @@ public interface ArticleVenduBLLManager {
 	public List<ArticleVendu> getAllArticleFilterCategorieAndNomArticle(String nomArticle, String categorie, List<ArticleVendu> lstArticleVendus) throws BLLException;
 	
 	public List<ArticleVendu> getAllArticleEncheresOuvertes() throws BLLException;
-//	
-//	public List<ArticleVendu> getAllArticleMesEncheres(Utilisateur user) throws BLLException;
-//	
-//	public List<ArticleVendu> getAllArticleMesEncheresRemportees(Utilisateur user) throws BLLException;
-//	
-//	public List<ArticleVendu> getAllArticleMesVentesEnCours(Utilisateur user) throws BLLException;
-//
-//	public List<ArticleVendu> getAllArticleVentesNonDebutees(Utilisateur user) throws BLLException;
-//	
-//	public List<ArticleVendu> getAllArticleVentesTerminees(Utilisateur user) throws BLLException;
+	
+	public List<ArticleVendu> getAllArticleMesEncheres(Utilisateur user) throws BLLException;
+	
+	public List<ArticleVendu> getAllArticleMesEncheresRemportees(Utilisateur user) throws BLLException;
+	
+	public List<ArticleVendu> getAllArticleMesVentesEnCours(Utilisateur user) throws BLLException;
+
+	public List<ArticleVendu> getAllArticleVentesNonDebutees(Utilisateur user) throws BLLException;
+	
+	public List<ArticleVendu> getAllArticleVentesTerminees(Utilisateur user) throws BLLException;
 	
 	// méthode pour radio Achats
 	public List<ArticleVendu> getAllArticleEOuvertesMesEMesERemportees(Utilisateur user) throws BLLException;
@@ -68,6 +68,8 @@ public interface ArticleVenduBLLManager {
 	public List<ArticleVendu> getAllArticleMesVEnCoursVTerminees(Utilisateur user) throws BLLException;
 	
 	public List<ArticleVendu> getAllArticleVNonDebuteesVTerminees(Utilisateur user) throws BLLException;
+	
+	public void autoUpdateEtatArticle() throws BLLException;
 	
 	public void removeArticleVendu(ArticleVendu articlevendu) throws BLLException;
 
